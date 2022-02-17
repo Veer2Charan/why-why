1378bdb05b36f85324cc92c74c0e20dd82056ad6
Veer2Charan <90391925+Veer2Charan@users.noreply.github.com>
2022-02-17 14:52:17 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+var mouseEvent=""
+
+canvas=
+document.getElementById("myCanvas");
+ctx=canvas.getContext("2d");
+
+var color=document.getElementById("color");
+var radius=document.getElementById("radius");
+var width=document.getElementById("width");
+addEventListener(my mousedown,mousedown)
+
+var mouseEvent="mousedown"
+addEventListener(my mouseup, mouseup)
+mouseup (e) {
+    var mouseEvent="mouseup"
+}
+addEventListener(my mousedown,mousedown)
+
+var mouseEvent="mouseup"
+addEventListener(my mouseleave, mouseleave)
+mouseleave (e) {
+    var mouseEvent="mouseleave"
+}
+addEventListener(my mousemove,mousemove)
+
+var mouseEvent="mousemove"
+addEventListener(my mousel, mouseup)
+mouseup (e) {
+    var mouseEvent="mouseup"
+}
+
+
